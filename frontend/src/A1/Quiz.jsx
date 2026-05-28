1cf0f1bf72c6f18bf5c4fe95da3eb749b5d9b5ca
Bhagyashri Shingade <shingadebhagyashri3714@gmail.com>
2026-05-28 10:32:59 +0530

    Fix case-sensitive auth import

--- frontend/src/A1/Quiz.jsx
+++ frontend/src/A1/Quiz.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { isAdmin } from "../utils/auth";
+import { isAdmin } from "../utils/Auth";
 import { useNavigate } from "react-router-dom";
 
 const QuizA1 = () => {
--- frontend/src/A2/Grammar.jsx
+++ frontend/src/A2/Grammar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { isAdmin } from "../utils/auth";
+import { isAdmin } from "../utils/Auth";
 
 const GrammarA2 = () => {
 
